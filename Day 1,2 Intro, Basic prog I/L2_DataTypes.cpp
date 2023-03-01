7c4cece7d83b2dc0d38072d9ddee2d6a97010c0b
alinabi19 <96679148+alinabi19@users.noreply.github.com>
2023-03-01 23:43:17 +0530

    Add files via upload

---
+++ Day 1,2 Intro, Basic prog I/L2_DataTypes.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+/*
+    int a=4;
+    char c='b';
+    float f=23.67;
+    double d=34.567;
+    bool b=true;
+
+    cout<<"Integer : "<<a<<endl
+    <<"Character : "<<c<<endl
+    <<"Float : "<<f<<endl
+    <<"Double : "<<d<<endl
+    <<"Boolean : "<<b<<endl;
+
+    cout<<endl;
+
+    cout<<"Size of Integer : "<<sizeof(a)<<endl
+    <<"Size of Character : "<<sizeof(c)<<endl
+    <<"Size of Float : "<<sizeof(f)<<endl
+    <<"Size of Double : "<<sizeof(d)<<endl
+    <<"Size of Boolean : "<<sizeof(b)<<endl;
+
+*/
+
+// Type Casting
+    int a='a';
+    cout<<a<<endl;
+
+    char c=98;
+    cout<<c<<endl;
+
+
+return 0;
+}
---
+++ Day 1,2 Intro, Basic prog I/L2_operators.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=2/5;
+    cout<<a<<endl;
+
+    int a1=9.0/10;
+    cout<<a1<<endl;
+
+    int b=2.0/5;
+    cout<<b<<endl;
+
+    cout<<2.0/5<<endl;
+
+    float f=2.0/5;
+    cout<<f<<endl;
+
+
+return 0;
+}
